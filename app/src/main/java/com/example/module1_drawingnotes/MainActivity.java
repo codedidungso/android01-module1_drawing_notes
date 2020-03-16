ace27a949523edd095fe16d347085341e6e14e69
qklahpita <lpita.91@gmail.com>
2020-03-16 23:32:02 +0700

    Session 3 (part 2): Permission

--- app/src/main/java/com/example/module1_drawingnotes/MainActivity.java
+++ app/src/main/java/com/example/module1_drawingnotes/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.module1_drawingnotes;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    int requestCodeWriteStorage = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +32,44 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        setupPermission();
+    }
+
+    private void setupPermission() {
+        String[] permissions = new String[1];
+        permissions[0] = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+        //new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                requestPermissions(permissions, requestCodeWriteStorage);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == requestCodeWriteStorage) {
+            if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Warning!")
+                        .setMessage("Without permission you can not use this app. Do you want to grant permission?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                setupPermission();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                MainActivity.this.finish();
+                            }
+                        })
+                        .show();
+            }
+        }
     }
 }
